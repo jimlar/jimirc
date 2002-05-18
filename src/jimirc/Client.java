92c51dce300707148e18441fabfdcb03316d0eb0
jimmy <jimmy>
2002-05-18 13:09:58 +0000

    started work on irc client

--- src/jimirc/Client.java
+++ src/jimirc/Client.java
@@ -1,8 +1,14 @@
 package jimirc;
 
 public class Client {
+    private IRCConnection connection;
+
+    public Client(String host, int port) {
+	this.connection = new IRCConnection(host, port);
+    }
 
 
-
-  
+    public static void main(String args[]) {
+	Client client = new Client("localhost", 6667);
+    }
 }
